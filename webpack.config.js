6160a479222711eba271026666ca22a7e4b29fe2
Jastin-Infj <takumiyokoo@gmail.com>
2023-06-02 23:43:47 +0900

    [setup] html-loader babel を導入 分割してファイル出力してほしいから

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
 const PATH = require('path');
 
 const OUTPUT_PATH = PATH.resolve(__dirname , 'dist');
+const HTML_WEBPACK_PLUGIN = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main_[hash].js',
     path: OUTPUT_PATH
   },
+  module: {
+    rules: [
+      {
+        test: /\.js?x$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HTML_WEBPACK_PLUGIN({
+      template: "./src/html/index.html",
+      filename: "./index.html"
+    }),
+  ],
   devServer: {
     static: {
       directory: PATH.join(__dirname, 'dist')
