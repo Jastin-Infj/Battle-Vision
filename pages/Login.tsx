3cd375297f554ed107c8f57cfc7400ca59a2936a
Jastin-Infj <takumiyokoo@gmail.com>
2023-06-22 20:01:09 +0900

    [add] #42 ヘッダー見た目完成

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 
+import Messages from '../src/frontend/json/Strings.json';
 import '../src/frontend/scss/login.scss';
 
 function Login() {
@@ -18,39 +19,149 @@ function Login() {
           <div className="header__icon_step">
             <div>
               <div className="stepflow"></div>
-              <span>
+              <span className="main">
                 ポケモンを追加
-                <span>ポケモンを追加</span>
+                <span className="back">ポケモンを追加</span>
               </span>
               <div className="arrow"></div>
             </div>
             <div>
               <div className="stepflow"></div>
-              <span>
+              <span className="main">
                 チームを登録
-                <span>チームを登録</span>
+                <span className="back">チームを登録</span>
               </span>
               <div className="arrow"></div>
             </div>
             <div>
               <div className="stepflow"></div>
-              <span>
+              <span className="main">
                 対戦を記録
-                <span>対戦を記録</span>
+                <span className="back">対戦を記録</span>
               </span>
               <div className="arrow"></div>
             </div>
             <div>
               <div className="stepflow"></div>
-              <span>
+              <span className="main">
                 対戦記録を見る
-                <span>対戦記録を見る</span>
+                <span className="back">対戦記録を見る</span>
               </span>
             </div>
           </div>
+          <div className="header__icon_login">
+            <div className="icon">
+              <img className="none"/>
+            </div>
+            <div className="logout disabled">
+              <span>
+                ログアウト
+                <span>ログアウト</span>
+              </span>
+            </div>
+            <div className="login disabled">
+              <span>
+                ログイン
+                <span>ログイン</span>
+              </span>
+            </div>
+          </div>
+          <div className="header__box_menu">
+            <ul>
+              <li>
+                <span className="main">
+                  マイページ
+                  <span className="back">マイページ</span>
+                </span>
+              </li>
+              <li>
+                <span className="main">
+                  ポケモン新規登録
+                  <span className="back">ポケモン新規登録</span>
+                </span>
+              </li>
+              <li>
+                <span className="main">
+                  ポケモン一覧
+                  <span className="back">ポケモン一覧</span>
+                </span>
+              </li>
+              <li>
+                <span className="main">
+                  チーム一覧
+                  <span className="back">チーム一覧</span>
+                </span>
+              </li>
+              <li>
+                <span className="main">
+                  対戦を記録
+                  <span className="back">対戦を記録</span>
+                </span>
+              </li>
+              <li>
+                <span className="main">
+                  成績結果を見る
+                  <span className="back">成績結果を見る</span>
+                </span>
+              </li>
+              <li>
+                <span className="main">
+                  デモデータを試す
+                  <span className="back">デモデータを試す</span>
+                </span>
+              </li>
+              <li>
+                <span className="main">
+                  チュートリアル
+                  <span className="back">チュートリアル</span>
+                </span>
+              </li>
+              <li>
+                <span className="main">
+                  ログアウト
+                  <span className="back">ログアウト</span>
+                </span>
+              </li>
+            </ul>
+          </div>
         </header>
         <main>main</main>
-        <footer>footer</footer>
+        <footer>
+          <div className="footer__text_messages">
+            <span>{Messages.Page.Top.Fotter.Messages.Text1}</span>
+            <span>{Messages.Page.Top.Fotter.Messages.Text2}</span>
+            <span>{Messages.Page.Top.Fotter.Messages.Text3}</span>
+          </div>
+          {/* <div className="footer__text_copyright">
+            <span>{Messages.Page.Top.Fotter.Copyright.Text1}</span>
+            <span>{Messages.Page.Top.Fotter.Copyright.Text2}</span>
+          </div> */}
+          {/* <div className="footer__button_accounts">
+            <span>{Messages.Page.Top.Fotter.Accounts.Text}</span>
+            <div>
+              <button className="SignUp">
+                <span>
+                  {Messages.Page.Top.Fotter.Accounts.SignUp}
+                </span>
+              </button>
+              <button className="SignIn">
+                <span>
+                  {Messages.Page.Top.Fotter.Accounts.SignIn}
+                </span>
+              </button>
+              <button className="Demo">
+                <span>
+                  {Messages.Page.Top.Fotter.Accounts.Demo}
+                </span>
+              </button>
+            </div>
+          </div> */}
+          {/* <div className="footer__link_info">
+            <a>{Messages.Page.Top.Fotter.Links.Text1}</a>
+            <a>{Messages.Page.Top.Fotter.Links.Text2}</a>
+            <a>{Messages.Page.Top.Fotter.Links.Text3}</a>
+          </div> */}
+        </footer>
       </div>
     </>
   );
