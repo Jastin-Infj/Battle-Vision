a86ff1376c0c3282dcc2bd2e1aeec26cb115e96c
Jastin-Infj <takumiyokoo@gmail.com>
2023-07-08 18:06:43 +0900

    [add] #50 開発途中まで

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -10,6 +10,7 @@ import Header from "../src/frontend/ts/components/Header";
 import Footer from "../src/frontend/ts/components/Footer";
 
 function Main() {
+
   return (
     <>
       <Canvas>
@@ -17,15 +18,45 @@ function Main() {
         <main>
           <div className="main__filter">
             <span className="main__text_filter">{Messages.Page.Main.Text.Filter}</span>
-            <div className="main__group_tag"></div>
+            <div className="main__group_tag">
+
+            </div>
             <div className="main__group_day">
               <span>{Messages.Page.Main.Text.Register}</span>
-              <div></div>
+              <input type="date"></input>
             </div>
             <div className="main__group_day">
               <span>{Messages.Page.Main.Text.Update}</span>
-              <div></div>
+              <input type="date"></input>
             </div>
+          </div>
+          <div className="main__group_winlose">
+            <span className="winlose__text">{Messages.Page.Main.Text.Winlose}</span>
+            <div className="winlose__box">
+              <div className="winlose__box_win">
+                <span>50%</span>
+              </div>
+              <div className="winlose__box_lose">
+                <span>50%</span>
+              </div>
+            </div>
+            <div className="winlose__buttons">
+              <button>
+                <span>{Messages.Page.Main.Text.Win}</span>
+              </button>
+              <button>
+                <span>{Messages.Page.Main.Text.Lose}</span>
+              </button>
+            </div>
+            <div className="winlose__reports">
+              <img />
+              <button>
+                <span>{Messages.Page.Main.Text.Report}</span>
+              </button>
+            </div>
+          </div>
+          <div className="main__group_battlecanvas">
+            
           </div>
         </main>
         <Footer />
