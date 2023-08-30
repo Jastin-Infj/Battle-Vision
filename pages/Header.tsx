43df5adf0a08841237200553dfbd349c2f6819cd
Jastin-Infj <takumiyokoo@gmail.com>
2023-08-30 16:37:00 +0900

    [add] ヘッダーテキスト リンク紐づけ

--- pages/Header.tsx
+++ pages/Header.tsx
@@ -1,74 +1,143 @@
 import React from "react";
-import { useSearchParams } from 'next/navigation';
+import { getCurrentQueryMode , getQueryRequestHref, getQueryURL } from "../src/frontend/ts/common";
 
 import Messages from '../src/frontend/json/Strings.json';
+import Links from '../src/frontend/json/link.json';
+
 import '../src/frontend/scss/_layerbase.scss';
+import TextStroke from "../src/frontend/ts/components/_textStroke";
 
 function Header() {
 
   //* デモモード確認
-  const searchParams = useSearchParams();
-  const QUERY_PARAM = searchParams.get('mode');
+  let QUERY_DEMO:string | null | string[] = getQueryURL(Links.Query["mode"].title);
 
-  const JSX_DemoMode = {
-    Demo: (
-      <>
-        <p>{Messages.Page.Header.Texts.Demo1}<br />{Messages.Page.Header.Texts.Demo2}</p>
-      </>
-    ),
-    None: (
-      <>
-        
-      </>
-    )
-  };
+  // 複数クエリある場合は取得
+  if(QUERY_DEMO) {
+    const multiQuery = QUERY_DEMO.split(" ");
+    if(multiQuery.length > 1) {
+      QUERY_DEMO = multiQuery;
+    }
+  }
+
+  //* クエリ関連
+  const key_mode = Links.Query["mode"].title;
+  const currentMode = getCurrentQueryMode(QUERY_DEMO,key_mode);
+
+  const JSX_DemoMode = () => {
+
+    let render: JSX.Element = null!;
+
+    switch(currentMode) {
+      case 'None':
+        render = (<></>);
+        break;
+      case 'demo':
+        render = (
+          <>
+            <p>{Messages.Page.Header.Texts.Demo1}<br />{Messages.Page.Header.Texts.Demo2}</p>
+          </>
+        );
+        break;
+    }
+    return render;
+  }
+
+  //* クエリ関係
+  const JSX_StepFlow = (stepNum: number) => {
+    
+    let render: JSX.Element = null!;
+    let text: string = null!;
+    let href: string = null!;
+
+    switch(stepNum) {
+      case 1:
+        text = Messages.Page.Header.Icon.Steps.Text1;
+        href = Links.Page.Header.Step1;
+        break;
+      case 2:
+        text = Messages.Page.Header.Icon.Steps.Text2;
+        href = Links.Page.Header.Step2;
+        break;
+      case 3:
+        text = Messages.Page.Header.Icon.Steps.Text3;
+        href = Links.Page.Header.Step3;
+        break;
+      case 4:
+        text = Messages.Page.Header.Icon.Steps.Text4;
+        href = Links.Page.Header.Step4;
+        break;
+      default:
+        new Error(Messages.FrontEnd.DevelopError["01"]);
+        break;
+    }
+
+    switch(currentMode) {
+      case 'None':
+        render = (
+          <>
+            <TextStroke 
+              text={text}
+              type="a" 
+              href={href} 
+            />
+          </>
+        );
+      break;
+      case 'demo':
+        href = getQueryRequestHref(href,Links.Query.mode.Demo);
+        render = (
+          <>
+            <TextStroke 
+              text={text}
+              type="a" 
+              href={href} 
+            />
+          </>
+        );
+      break;
+    }
+
+    return render;
+  }
 
   return (
     <>
       <header>
+        <input type="checkbox" name={"header__icon"} id={"header__icon"} />
         <div className="header__text_Demo">
-          {QUERY_PARAM === "demo" ? JSX_DemoMode.Demo  : JSX_DemoMode.None}
+          {JSX_DemoMode()}
         </div>
         <div className="header__textbox_search">
           <img />
           <input type="text" placeholder={Messages.Page.Header.Textbox.search} id="name" />
         </div>
         <div className="header__icon_step">
-          <div>
+          <div className="row">
+            <div className="stepflow" />
+            {JSX_StepFlow(1)}
+            <div className="arrow" />
+          </div>
+          <div className="row">
             <div className="stepflow"></div>
-            <span className="main">
-              {Messages.Page.Header.Icon.Steps.Text1}
-              <span className="back">{Messages.Page.Header.Icon.Steps.Text1}</span>
-            </span>
+            {JSX_StepFlow(2)}
             <div className="arrow"></div>
           </div>
-          <div>
+          <div className="row">
             <div className="stepflow"></div>
-            <span className="main">
-            {Messages.Page.Header.Icon.Steps.Text2}
-              <span className="back">{Messages.Page.Header.Icon.Steps.Text2}</span>
-            </span>
+            {JSX_StepFlow(3)}
             <div className="arrow"></div>
           </div>
-          <div>
+          <div className="row">
             <div className="stepflow"></div>
-            <span className="main">
-            {Messages.Page.Header.Icon.Steps.Text3}
-              <span className="back">{Messages.Page.Header.Icon.Steps.Text3}</span>
-            </span>
-            <div className="arrow"></div>
-          </div>
-          <div>
-            <div className="stepflow"></div>
-            <span className="main">
-            {Messages.Page.Header.Icon.Steps.Text4}
-              <span className="back">{Messages.Page.Header.Icon.Steps.Text4}</span>
-            </span>
+            {JSX_StepFlow(4)}
           </div>
         </div>
         <div className="header__icon_login">
           <div className="icon">
-            <img className="none"/>
+            <label htmlFor={"header__icon"} id={"header__icon"}>
+              <img className="none" id={"header__icon"} />
+            </label>
           </div>
           <div className="logout disabled">
             <span>
--- src/frontend/ts/components/_textStroke.tsx
+++ src/frontend/ts/components/_textStroke.tsx
@@ -1,15 +1,51 @@
 import React from "react";
+import Link from "next/link";
+import { Url } from "next/dist/shared/lib/router/router";
 
-interface Props {
+interface PropBase {
   text: string;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
 }
 
+interface Span_TAG extends PropBase {
+  type?: 'span';
+}
+
+interface A_TAG extends PropBase {
+  type: 'a';
+  href: Url;
+}
+
+type Props = Span_TAG | A_TAG;
+
 function TextStroke(props:Props) {
+
+  let render: JSX.Element = null!;
+
+  switch(props.type) {
+    case 'a':
+      render = (
+        <>
+          <Link className="main" href={props.href} >{props.text}</Link>
+          <Link className="back" href={""}>{props.text}</Link>
+        </>
+      );
+      break;
+    case 'span':
+    default:
+      render = (
+        <>
+          <span className="main">{props.text}</span>
+          <span className="back">{props.text}</span>
+        </>
+      );
+      break;
+  }
+
   return (
     <>
-      <div className="stroke-text">
-        <span className="main">{props.text}</span>
-        <span className="back">{props.text}</span>
+      <div className="stroke-text" onClick={props.onClick}>
+        {render}
       </div>
     </>
   );
