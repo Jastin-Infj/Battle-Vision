de80820140e5e394859432f9bf515e6c17dd3651
Jastin-Infj <takumiyokoo@gmail.com>
2023-09-04 20:27:45 +0900

    [add] #86 Django テスト導入

--- src/hello/app/app/urls.py
+++ src/hello/app/app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangoEnv project.
+URL configuration for app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,21 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-from rest_framework import routers
-from ServerSide import views
-
-router = routers.DefaultRouter()
-router.register(r'users',views.UserView,'ServerSide')
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('django/', include('ServerSide.urls')),
-    path('api/',include(router.urls)),
 ]
-
-urlpatterns += static(settings.STATIC_URL , document_root=settings.STATICFILES_DIRS)
